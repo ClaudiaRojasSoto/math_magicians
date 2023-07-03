0c88289c7b71728c54dfc8332737cea07e276c58
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-07-03 17:43:40 -0400

    Create the folder components inside the src folder, and create the Calculator.js file inside it

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,32 +1,38 @@
 import React from 'react';
+import './calculator.css';
 
 function Calculator() {
   return (
     <div className="calculator">
       <div className="display">0</div>
       <div className="row">
-        <button className="button">7</button>
-        <button className="button">8</button>
-        <button className="button">9</button>
-        <button className="button">÷</button>
+        <button className="button clear" type="button">AC</button>
+        <button className="button" type="button">+/-</button>
+        <button className="button" type="button">%</button>
+        <button className="button operator" type="button">/</button>
       </div>
       <div className="row">
-        <button className="button">4</button>
-        <button className="button">5</button>
-        <button className="button">6</button>
-        <button className="button">x</button>
+        <button className="button" type="button">1</button>
+        <button className="button" type="button">2</button>
+        <button className="button" type="button">3</button>
+        <button className="button operator" type="button">+</button>
       </div>
       <div className="row">
-        <button className="button">1</button>
-        <button className="button">2</button>
-        <button className="button">3</button>
-        <button className="button">-</button>
+        <button className="button" type="button">4</button>
+        <button className="button" type="button">5</button>
+        <button className="button" type="button">6</button>
+        <button className="button operator" type="button">-</button>
       </div>
       <div className="row">
-        <button className="button">0</button>
-        <button className="button">.</button>
-        <button className="button">=</button>
-        <button className="button">+</button>
+        <button className="button" type="button">7</button>
+        <button className="button" type="button">8</button>
+        <button className="button" type="button">9</button>
+        <button className="button operator" type="button">x</button>
+      </div>
+      <div className="row">
+        <button className="button" type="button">0</button>
+        <button className="button" type="button">+</button>
+        <button className="button operator" type="button">=</button>
       </div>
     </div>
   );
