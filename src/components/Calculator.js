cf549143222789fdcaee60fa3ffc15183329c134
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-07-03 14:53:37 -0400

    Create the components folder and inside it Calculator.js file and add it the calculator skeleton

---
+++ src/components/Calculator.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Calculator() {
+  return (
+    <div className="calculator">
+      <div className="display">0</div>
+      <div className="row">
+        <button className="button">7</button>
+        <button className="button">8</button>
+        <button className="button">9</button>
+        <button className="button">÷</button>
+      </div>
+      <div className="row">
+        <button className="button">4</button>
+        <button className="button">5</button>
+        <button className="button">6</button>
+        <button className="button">x</button>
+      </div>
+      <div className="row">
+        <button className="button">1</button>
+        <button className="button">2</button>
+        <button className="button">3</button>
+        <button className="button">-</button>
+      </div>
+      <div className="row">
+        <button className="button">0</button>
+        <button className="button">.</button>
+        <button className="button">=</button>
+        <button className="button">+</button>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
