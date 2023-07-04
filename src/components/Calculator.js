8a0614b455ae670c4e73d6f2382be6df88c88ca0
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-07-04 11:59:42 -0400

    Add the handlers to control clic events on buttons to Calculator-js file  using the providen logic on calculate.js and operate.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './calculator.css';
+import calculate from '../logic/calculate';
 
 export function Display(props) {
   const { value } = props;
@@ -12,9 +13,9 @@ Display.propTypes = {
 };
 
 export function Button(props) {
-  const { value, type } = props;
+  const { value, type, onClick } = props;
   return (
-    <button className={`button ${type}`} type="button">
+    <button className={`button ${type}`} type="button" onClick={onClick}>
       {value}
     </button>
   );
@@ -23,6 +24,7 @@ export function Button(props) {
 Button.propTypes = {
   value: PropTypes.string.isRequired,
   type: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
 };
 
 Button.defaultProps = {
@@ -30,39 +32,50 @@ Button.defaultProps = {
 };
 
 function Calculator() {
-  const [displayValue] = useState('0');
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  function handleClick(buttonName) {
+    setCalculatorData((prevState) => calculate(prevState, buttonName));
+  }
+
+  const { total, next } = calculatorData;
+  const displayValue = next || total || '0';
 
   return (
     <div className="calculator">
       <Display value={displayValue} />
       <div className="row">
-        <Button value="AC" type="clear" />
-        <Button value="+/-" />
-        <Button value="%" />
-        <Button value="/" type="operator" />
+        <Button value="AC" type="clear" onClick={() => handleClick('AC')} />
+        <Button value="+/-" onClick={() => handleClick('+/-')} />
+        <Button value="%" onClick={() => handleClick('%')} />
+        <Button value="÷" type="operator" onClick={() => handleClick('÷')} />
       </div>
       <div className="row">
-        <Button value="1" />
-        <Button value="2" />
-        <Button value="3" />
-        <Button value="+" type="operator" />
+        <Button value="1" onClick={() => handleClick('1')} />
+        <Button value="2" onClick={() => handleClick('2')} />
+        <Button value="3" onClick={() => handleClick('3')} />
+        <Button value="+" type="operator" onClick={() => handleClick('+')} />
       </div>
       <div className="row">
-        <Button value="4" />
-        <Button value="5" />
-        <Button value="6" />
-        <Button value="-" type="operator" />
+        <Button value="4" onClick={() => handleClick('4')} />
+        <Button value="5" onClick={() => handleClick('5')} />
+        <Button value="6" onClick={() => handleClick('6')} />
+        <Button value="-" type="operator" onClick={() => handleClick('-')} />
       </div>
       <div className="row">
-        <Button value="7" />
-        <Button value="8" />
-        <Button value="9" />
-        <Button value="x" type="operator" />
+        <Button value="7" onClick={() => handleClick('7')} />
+        <Button value="8" onClick={() => handleClick('8')} />
+        <Button value="9" onClick={() => handleClick('9')} />
+        <Button value="x" type="operator" onClick={() => handleClick('x')} />
       </div>
       <div className="row">
-        <Button value="0" type="zero" />
-        <Button value="." />
-        <Button value="=" type="operator" />
+        <Button value="0" type="zero" onClick={() => handleClick('0')} />
+        <Button value="." onClick={() => handleClick('.')} />
+        <Button value="=" type="operator" onClick={() => handleClick('=')} />
       </div>
     </div>
   );
