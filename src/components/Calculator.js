02e07f7230dd29190f89a6b7334a106905f05465
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-07-03 20:00:57 -0400

    Add Display and Button as childs components on Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,63 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './calculator.css';
+
+export function Display() {
+  return <div className="display">0</div>;
+}
+
+export function Button(props) {
+  const { value, type, onClick } = props;
+  return (
+    <button className={`button ${type}`} type="button" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  value: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  type: '',
+  onClick: () => {},
+};
 
 function Calculator() {
   return (
     <div className="calculator">
-      <div className="display">0</div>
+      <Display />
       <div className="row">
-        <button className="button clear" type="button">AC</button>
-        <button className="button" type="button">+/-</button>
-        <button className="button" type="button">%</button>
-        <button className="button operator" type="button">/</button>
+        <Button value="AC" type="clear" />
+        <Button value="+/-" />
+        <Button value="%" />
+        <Button value="/" type="operator" />
       </div>
       <div className="row">
-        <button className="button" type="button">1</button>
-        <button className="button" type="button">2</button>
-        <button className="button" type="button">3</button>
-        <button className="button operator" type="button">+</button>
+        <Button value="1" />
+        <Button value="2" />
+        <Button value="3" />
+        <Button value="+" type="operator" />
       </div>
       <div className="row">
-        <button className="button" type="button">4</button>
-        <button className="button" type="button">5</button>
-        <button className="button" type="button">6</button>
-        <button className="button operator" type="button">-</button>
+        <Button value="4" />
+        <Button value="5" />
+        <Button value="6" />
+        <Button value="-" type="operator" />
       </div>
       <div className="row">
-        <button className="button" type="button">7</button>
-        <button className="button" type="button">8</button>
-        <button className="button" type="button">9</button>
-        <button className="button operator" type="button">x</button>
+        <Button value="7" />
+        <Button value="8" />
+        <Button value="9" />
+        <Button value="x" type="operator" />
       </div>
       <div className="row">
-        <button className="button" type="button">0</button>
-        <button className="button" type="button">+</button>
-        <button className="button operator" type="button">=</button>
+        <Button value="0" type="zero" />
+        <Button value="+" />
+        <Button value="=" type="operator" />
       </div>
     </div>
   );
