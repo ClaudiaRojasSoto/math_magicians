511a44bf62a66304751d6c3ed7367f66a99b7afa
ClaudiaRojasSoto <claudiarojassoto@gmail.com>
2023-07-04 15:08:41 -0400

    Include the component Quote on App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Calculator />
+        <Quote />
       </header>
     </div>
   );
