4f88e97b27dfa329b5e6504193ed6eaa0d3b73a5
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-07-10 21:52:42 -0400

    Add the navbar into the App function on App.js file inside components folder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes,
+} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Calculator />
-        <Quote />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <h1 className="logo">Math Magicians</h1>
+            {/* Estilo del título */}
+            <ul className="nav-links">
+              <li>
+                <Link to="/" className="nav-link">Home</Link>
+                {/* Estilo de los enlaces */}
+              </li>
+              <li>
+                <Link to="/calculator" className="nav-link">Calculator</Link>
+              </li>
+              <li>
+                <Link to="/quote" className="nav-link">Quote</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
